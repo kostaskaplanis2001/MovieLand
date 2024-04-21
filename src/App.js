f217562c635972d5c8ef8101ecc0be5295b64ffd
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-21 15:33:15 +0300

    Added useState and mapping through all movies to display them

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
@@ -15,10 +15,12 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -36,10 +38,17 @@ const App = () => {
         />
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
-
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 };
